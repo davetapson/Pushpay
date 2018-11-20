ccc83064e49a19bbdcaf42a21671e18f2ee651d3
davetapson <dave.tapson@gmail.com>
2018-11-20 21:56:45 +1300

    Add project files.

---
+++ Pushpay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pushpay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
